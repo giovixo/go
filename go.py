d76f77d14c57c65eb51b50b8aaeff13215b36f09
giovixo <decesare.giovanni@gmail.com>
2020-12-19 10:34:03 +0100

    colors everywhere

--- go.py
+++ go.py
@@ -2,7 +2,7 @@ import sys
 import os
 import yaml
 
-with open("/Users/giovanni/sw/scripts/dirs.yaml", 'r') as stream:
+with open("/Users/giovanni/Software/scripts/go/dirs.yaml", 'r') as stream:
     try:
         options = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
@@ -10,22 +10,22 @@ with open("/Users/giovanni/sw/scripts/dirs.yaml", 'r') as stream:
 
 if len(sys.argv) == 1:
     print("Usage: python go.py option")
-    print("Example: 'python go.py ml'")
+    print("Example: 'python go.py gammapy'")
     print("To get the list of the options: 'python go.py list'")
-    print("Configuration file: 'sw/script/dirs.yaml'")
+    print("Configuration file: 'Software/scripts/go/dirs.yaml'")
     exit() 
 
 if sys.argv[1] == 'list':
     print('Possible options:')
     for opt in options:
-        print('Option: ', opt['opt'])
-        print(opt['info'])
+        print('Option: \033[92m', opt['opt'], '\033[0m')
+        print('\033[96m' + opt['info'] + '\033[0m')
     exit()
 
 try:
     my_option = [x for x in options if x['opt'] == sys.argv[1]][0]
     os.chdir(my_option['dir'])
-    print(my_option['info'])
+    print('\033[96m' + my_option['info'] + '\033[0m')
     if my_option['notes'] != None:
         print("Notes: ", my_option['notes'])
     print("Working dir: {}".format(my_option['dir']))
